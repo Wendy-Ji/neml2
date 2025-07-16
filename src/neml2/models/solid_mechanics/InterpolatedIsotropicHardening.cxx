cb70e781adfb43f27e7cf6d32fa4e8efa057e116
Wendy-Ji <j.wendy234@gmail.com>
2025-07-17 08:13:53 +1000

    general update

--- src/neml2/models/solid_mechanics/InterpolatedIsotropicHardening.cxx
+++ src/neml2/models/solid_mechanics/InterpolatedIsotropicHardening.cxx
@@ -24,7 +24,7 @@
 
 #include "neml2/models/solid_mechanics/InterpolatedIsotropicHardening.h"
 #include "neml2/tensors/Scalar.h"
-#include "neml2/tensors/functions/exp.h"
+#include "neml2/tensors/indexing.h"
 
 namespace neml2
 {
@@ -45,6 +45,7 @@ InterpolatedIsotropicHardening::expected_options()
 
   options.set<std::vector<VariableName>>("stresses");
   options.set("stresses").doc() = "Stress list";
+
   options.set_parameter<std::vector<TensorName<Scalar>>>("hf_temperatures");
   options.set("hf_temperatures").doc() = "List of temperatures for each stress";
 
@@ -56,7 +57,7 @@ InterpolatedIsotropicHardening::expected_options()
 
 InterpolatedIsotropicHardening::InterpolatedIsotropicHardening(const OptionSet & options)
   : Model(options),
-    _temp_var(this->template declare_input_variable<Scalar>("temp_argument")),
+    _temp_var(declare_input_variable<Scalar>("temp_argument")),
     _h(declare_output_variable<Scalar>("isotropic_hardening"))
 {
   for (const auto & fv : options.get<std::vector<VariableName>>("stresses"))
@@ -73,37 +74,83 @@ InterpolatedIsotropicHardening::InterpolatedIsotropicHardening(const OptionSet &
 void
 InterpolatedIsotropicHardening::set_value(bool out, bool dout_din, bool d2out_din2)
 {
-  // current temperature
-  const auto temp = Scalar(this->_temp_var);
+  auto slope = Scalar::zeros(_stress.size() - 1);
+  auto X0 = Scalar::zeros(_stress.size() - 1);
+  auto X1 = Scalar::zeros(_stress.size() - 1);
+  auto Y0 = Scalar::zeros(_stress.size() - 1);
 
-  // interpolation
-  const auto ind_lo = 0;
-  const auto ind_hi = 1;
-  const auto frac = (temp - *_hf_temps[ind_lo]) / (*_hf_temps[ind_hi] - *_hf_temps[ind_lo]);
+  // auto stress_dev_lo = Scalar::zeros(_stress.size() - 1);
+  // auto stress_dev = Scalar::zeros(_stress.size());
+
+  for (std::size_t i = 0; i < _stress.size() - 1; i++)
+  {
+    slope[i] = (*_stress[i + 1] - *_stress[i]) / (*_hf_temps[i + 1] - *_hf_temps[i]);
+    X0[i] = *_hf_temps[i];
+    X1[i] = *_hf_temps[i + 1];
+    Y0[i] = *_stress[i];
+
+    // stress_dev_lo = (*_hf_temps[i + 1] - _temp_var) / (*_hf_temps[i + 1] - *_hf_temps[i]);
+  }
+
+  const auto loc = Scalar(at::logical_and(at::gt(_temp_var, X0), at::le(_temp_var, X1)));
+
+  const auto si = Scalar(slope.index({loc}));
+
+  // auto ind_lo = Scalar::linspace();
+
+  // stress_dev.index({loc}) = stress_dev_lo.index({loc});
+
+  // works for two values
+  // const auto ind_lo = 0;
+  // const auto ind_hi = 1;
+  // const auto frac = (_temp_var - *_hf_temps[ind_lo]) / (*_hf_temps[ind_hi] - *_hf_temps[ind_lo]);
 
   if (out)
   {
-    _h = (1 - frac) * (*_stress[ind_lo]) + frac * (*_stress[ind_hi]);
+    const auto X0i = Scalar(X0.index({loc}));
+    const auto Y0i = Scalar(Y0.index({loc}));
+    _h = Y0i + si * (_temp_var - X0i);
+    // works for two values
+    // _h = (1 - frac) * (*_stress[ind_lo]) + frac * (*_stress[ind_hi]);
   }
 
   if (dout_din)
   {
-    for (std::size_t i = 0; i < _stress.size(); i++)
-    {
-      if (_stress[i]->is_dependent())
-      {
-        if (i == ind_lo)
-          _h.d(*_stress[i]) = 1 - frac;
-        else if (i == ind_hi)
-          _h.d(*_stress[i]) = frac;
-        else
-          _h.d(*_stress[i]) = neml2::Scalar::full(0.0);
-      }
-    }
-
     if (_temp_var.is_dependent())
-      _h.d(_temp_var) =
-          (*_stress[ind_hi] - *_stress[ind_lo]) / (*_hf_temps[ind_hi] - *_hf_temps[ind_lo]);
+      _h.d(_temp_var) = si;
+
+    // for (std::size_t i = 0; i < _stress.size(); i++)
+    // {
+    //   if (_stress[i]->is_dependent())
+    //   {
+    //     _h.d(*_stress[i]) = Scalar(stress_dev[i]);
+    //   }
+    // }
+
+    // the derivatives
+    if (_stress[0]->is_dependent())
+      _h.d(*_stress[0]) = (*_hf_temps[1] - _temp_var) / (*_hf_temps[1] - *_hf_temps[0]);
+
+    if (_stress[1]->is_dependent())
+      _h.d(*_stress[1]) = (_temp_var - *_hf_temps[0]) / (*_hf_temps[1] - *_hf_temps[0]);
+
+    // works for two values
+    // for (std::size_t i = 0; i < _stress.size(); i++)
+    // {
+    //   if (_stress[i]->is_dependent())
+    //   {
+    //     if (i == ind_lo)
+    //       _h.d(*_stress[i]) = 1 - frac;
+    //     else if (i == ind_hi)
+    //       _h.d(*_stress[i]) = frac;
+    //     else
+    //       _h.d(*_stress[i]) = neml2::Scalar::full(0.0);
+    //   }
+    // }
+
+    // if (_temp_var.is_dependent())
+    //   _h.d(_temp_var) =
+    //       (*_stress[ind_hi] - *_stress[ind_lo]) / (*_hf_temps[ind_hi] - *_hf_temps[ind_lo]);
   }
 
   if (d2out_din2)
